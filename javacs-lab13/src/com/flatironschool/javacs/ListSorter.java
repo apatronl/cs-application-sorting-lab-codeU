2ecf39407a92c3d238af1c1079004c17d0c6c6f6
apatronl <apl7@gatech.edu>
2016-07-17 23:21:04 -0700

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.flatironschool.javacs;
 
@@ -18,13 +18,13 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public void insertionSort(List<T> list, Comparator<T> comparator) {
-	
+
 		for (int i=1; i < list.size(); i++) {
 			T elt_i = list.get(i);
 			int j = i;
@@ -42,7 +42,7 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
@@ -55,58 +55,118 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * Returns a list that might be new.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+		int size = list.size();
+		if (size <= 1) {
+			return list;
+		}
+
+		List<T> first = mergeSort(new LinkedList<T>(list.subList(0, size/2)), comparator);
+		List<T> second = mergeSort(new LinkedList<T>(list.subList(size/2, size)), comparator);
+
+		return merge(first, second, comparator);
 	}
 
 	/**
+	 * Merges two sorted lists into a single sorted list.
+	 *
+	 * @param first
+	 * @param second
+	 * @param comparator
+	 * @return
+	 */
+	private List<T> merge(List<T> first, List<T> second, Comparator<T> comparator) {
+		List<T> result = new LinkedList<T>();
+		int total = first.size() + second.size();
+		for (int i=0; i<total; i++) {
+			List<T> smallest = smallest(first, second, comparator);
+			result.add(smallest.remove(0));
+		}
+		return result;
+	}
+
+	private List<T> smallest(List<T> first, List<T> second, Comparator<T> comparator) {
+		if (first.size() == 0) {
+			return second;
+		}
+		if (second.size() == 0) {
+			return first;
+		}
+		if (comparator.compare(first.get(0), second.get(0)) <= 0) {
+			return first;
+		}
+		return second;
+	}
+
+
+	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+		PriorityQueue<T> queue = new PriorityQueue<T>(list.size(), comparator);
+		for (T element : list) {
+			queue.offer(element);
+		}
+		list.clear();
+		while (!queue.isEmpty()) {
+			list.add((T) queue.poll());
+		}
 	}
 
-	
+
 	/**
 	 * Returns the largest `k` elements in `list` in ascending order.
-	 * 
+	 *
 	 * @param k
 	 * @param list
 	 * @param comparator
-	 * @return 
+	 * @return
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        List<T> topK = new ArrayList<T>();
+		PriorityQueue<T> queue = new PriorityQueue<T>(list.size(), comparator);
+		for (T element : list) {
+			if (queue.size() < k) {
+				queue.offer(element);
+			} else {
+				if (comparator.compare(element, queue.peek()) > 0) {
+					queue.poll();
+					queue.offer(element);
+				}
+			}
+		}
+		while (!queue.isEmpty()) {
+			topK.add(queue.poll());
+		}
+        return topK;
 	}
 
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(3, 5, 1, 4, 2));
-		
+
 		Comparator<Integer> comparator = new Comparator<Integer>() {
 			@Override
 			public int compare(Integer n, Integer m) {
 				return n.compareTo(m);
 			}
 		};
-		
+
 		ListSorter<Integer> sorter = new ListSorter<Integer>();
 		sorter.insertionSort(list, comparator);
 		System.out.println(list);
@@ -118,7 +178,7 @@ public class ListSorter<T> {
 		list = new ArrayList<Integer>(Arrays.asList(3, 5, 1, 4, 2));
 		sorter.heapSort(list, comparator);
 		System.out.println(list);
-	
+
 		list = new ArrayList<Integer>(Arrays.asList(6, 3, 5, 8, 1, 4, 2, 7));
 		List<Integer> queue = sorter.topK(4, list, comparator);
 		System.out.println(queue);
